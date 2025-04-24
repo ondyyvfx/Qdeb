4717a32bc2f190267e50df7330af57b3e149647d
ondyy <ondyyvfx@gmail.com>
2025-04-24 23:27:30 +0500

    update

---
+++ lib/heroicons-react.d.ts
@@ -0,0 +1,5 @@
+declare module '@heroicons/react/solid' {
+    export * from '@heroicons/react/20/solid'; // If using the 20px version
+    // If using other versions, you can adjust accordingly.
+  }
+  
