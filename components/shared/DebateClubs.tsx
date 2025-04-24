7fcc6b85124afe1cce56d19b922797f23449463a
ondyy <ondyyvfx@gmail.com>
2025-04-24 23:22:26 +0500

    modified swiper

--- components/shared/DebateClubs.tsx
+++ components/shared/DebateClubs.tsx
@@ -4,7 +4,7 @@ import { Card } from '@/components/ui/card';
 import zhetiImage from '@/public/assets/debateClubs/zheti.png';
 import parasatImage from '@/public/assets/debateClubs/parasat.png';
 import mangilikImage from '@/public/assets/debateClubs/mangilik.png';
-import logo from '@/public/assets/logo.svg';
+import Qlogo from '@/public/assets/Qclub.svg';
 
 type DebateClub = {
   name: string;
@@ -58,12 +58,23 @@ const DebateClubs = () => {
       <h2 className="text-3xl font-bold mb-6">Дебатные клубы</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {debateClubs.map((club, index) => (
-          <Card key={index} className="bg-primary border-none flex flex-col p-4 gap-4 items-center">
-            <div className="flex w-full gap-4 items-center">
+          <Card
+          key={index}
+          className="bg-primary border-none flex flex-col p-10 gap-4 relative overflow-hidden"
+          style={{
+            backgroundImage: `url(${Qlogo.src})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'right bottom',
+            backgroundSize: '40%',
+          }}
+        >
+            <div className="flex w-full gap-4 items-center"
+              
+            >
               <img
                 src={club.image}
                 alt={club.name}
-                className="w-24 h-24 object-cover rounded-lg"
+                className="w-18 h-18 object-cover rounded-lg"
               />
               <div className="flex flex-col">
                 <h3 className="text-lg font-semibold">{club.name}</h3>
@@ -71,7 +82,7 @@ const DebateClubs = () => {
                 <p className="text-sm">Рейтинг: {club.rating} / 5</p>
               </div>
             </div>
-            <Button className="bg-secondary hover:bg-secondary/20 mt-2 w-full" size="sm">
+            <Button className="border bg-primary hover:bg-secondary/20 mt-2 w-[150px] h-10 rounded-lg">
               Подробнее
             </Button>
           </Card>
