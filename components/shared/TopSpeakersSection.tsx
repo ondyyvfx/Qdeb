784792ceca66b9453faaa2978e6269f964bcd619
ondyy <ondyyvfx@gmail.com>
2025-04-26 23:47:53 +0500

    top speaker section modification

---
+++ components/shared/TopSpeakersSection.tsx
@@ -0,0 +1,12 @@
+import SpeakerSlider from "./SpeakerSlider";
+
+const TopSpeakersSection = () => {
+  return (
+    <section className="my-24 mx-19">
+      <h1 className="text-3xl font-bold mb-6">Лучшие спикеры</h1>
+      <SpeakerSlider />
+    </section>
+  );
+};
+
+export default TopSpeakersSection;
