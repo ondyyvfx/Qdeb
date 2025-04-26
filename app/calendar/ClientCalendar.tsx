d0ef2ecdd5214ee87fd5f911cff5b229784f0b7e
ondyy <ondyyvfx@gmail.com>
2025-04-26 23:40:29 +0500

    client optimization

--- app/calendar/ClientCalendar.tsx
+++ app/calendar/ClientCalendar.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { format, parseISO } from 'date-fns';
-import { ru } from 'date-fns/locale/ru';
+import React, { useEffect, useState } from "react";
+import { format, parseISO } from "date-fns";
+import { ru } from "date-fns/locale/ru";
 import { useMemo } from "react";
 
 type Event = {
@@ -12,6 +12,7 @@ type Event = {
   cost: number;
   city: string;
   event_datetime: string;
+  is_registration_open: boolean;
   formattedDate?: string;
   formattedDay?: string;
   formattedMonth?: string;
@@ -22,35 +23,42 @@ type ClientCalendarProps = {
 };
 
 const ClientCalendar = ({ events }: ClientCalendarProps) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [selectedCity, setSelectedCity] = useState('');
-  const [formattedEvents, setFormattedEvents] = useState<Record<string, Event[]>>({});
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [formattedEvents, setFormattedEvents] = useState<
+    Record<string, Event[]>
+  >({});
 
   useEffect(() => {
     const formatEvents = () => {
-      const monthMap: Record<string, { displayMonth: string; events: Event[] }> = {};
+      const monthMap: Record<
+        string,
+        { displayMonth: string; events: Event[] }
+      > = {};
 
-      Object.values(events).flat().forEach((event) => {
-        const rawDate = event.event_datetime.split('T')[0];
-        const date = parseISO(rawDate);
+      Object.values(events)
+        .flat()
+        .forEach((event) => {
+          const rawDate = event.event_datetime.split("T")[0];
+          const date = parseISO(rawDate);
 
-        const monthKey = format(date, 'yyyy-MM');
-        const displayMonth = format(date, 'LLLL yyyy', { locale: ru });
-        const formattedDay = format(date, 'd');
+          const monthKey = format(date, "yyyy-MM");
+          const displayMonth = format(date, "LLLL yyyy", { locale: ru });
+          const formattedDay = format(date, "d");
 
-        const formattedEvent: Event = {
-          ...event,
-          formattedDate: format(date, 'dd.MM.yyyy'),
-          formattedDay,
-          formattedMonth: displayMonth,
-        };
+          const formattedEvent: Event = {
+            ...event,
+            formattedDate: format(date, "dd.MM.yyyy"),
+            formattedDay,
+            formattedMonth: displayMonth,
+          };
 
-        if (!monthMap[monthKey]) {
-          monthMap[monthKey] = { displayMonth, events: [] };
-        }
+          if (!monthMap[monthKey]) {
+            monthMap[monthKey] = { displayMonth, events: [] };
+          }
 
-        monthMap[monthKey].events.push(formattedEvent);
-      });
+          monthMap[monthKey].events.push(formattedEvent);
+        });
 
       const sortedKeys = Object.keys(monthMap).sort();
 
@@ -66,26 +74,33 @@ const ClientCalendar = ({ events }: ClientCalendarProps) => {
     formatEvents();
   }, [events]);
 
-  const filteredEvents = Object.entries(formattedEvents).map(([month, eventsList]) => {
-    const sortedEvents = [...eventsList].sort((a, b) =>
-      new Date(a.event_datetime).getTime() - new Date(b.event_datetime).getTime()
-    );
-  
-    const filtered = sortedEvents.filter((event) => {
-      const matchesSearchTerm = event.title.toLowerCase().includes(searchTerm.toLowerCase());
-      const matchesCity = selectedCity ? event.city.toLowerCase().includes(selectedCity.toLowerCase()) : true;
-      return matchesSearchTerm && matchesCity;
-    });
-  
-    return { month, events: filtered };
-  });
+  const filteredEvents = Object.entries(formattedEvents).map(
+    ([month, eventsList]) => {
+      const sortedEvents = [...eventsList].sort(
+        (a, b) =>
+          new Date(a.event_datetime).getTime() -
+          new Date(b.event_datetime).getTime()
+      );
+
+      const filtered = sortedEvents.filter((event) => {
+        const matchesSearchTerm = event.title
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+        const matchesCity = selectedCity
+          ? event.city.toLowerCase().includes(selectedCity.toLowerCase())
+          : true;
+        return matchesSearchTerm && matchesCity;
+      });
+
+      return { month, events: filtered };
+    }
+  );
 
   const uniqueCities = useMemo(() => {
     const allEvents = Object.values(formattedEvents).flat();
     const cities = new Set(allEvents.map((event) => event.city));
     return ["Все", ...Array.from(cities)];
   }, [formattedEvents]);
-  
 
   return (
     <div>
@@ -102,13 +117,13 @@ const ClientCalendar = ({ events }: ClientCalendarProps) => {
           value={selectedCity}
           onChange={(e) => setSelectedCity(e.target.value)}
           className="p-2 rounded-lg bg-primary text-white w-1/3 appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
-          style={{color: '#fff' }}
+          style={{ color: "#fff" }}
         >
           {uniqueCities.map((city) => (
             <option
               key={city}
               value={city === "Все" ? "" : city}
-              style={{ color: '#fff' }}
+              style={{ color: "#fff" }}
             >
               {city}
             </option>
@@ -117,61 +132,90 @@ const ClientCalendar = ({ events }: ClientCalendarProps) => {
       </div>
 
       {/* Список мероприятий */}
-      {filteredEvents.map(({ month, events }) => (
-        events.length > 0 && (
-          <div key={month} className="mb-9">
-            <h2 className="text-2xl font-semibold mb-4 capitalize">{month}</h2>
-            <div className="space-y-4">
-              {events.map((event) => (
-                <div key={event.id} className="relative bg-primary p-6 py-4 rounded-2xl shadow-lg flex gap-6">
-                  
-                  <div className="border-[0.5px] mx-4 px-4 py-2 rounded-sm absolute top-9 right-4 text-sm font-medium text-green-400 z-10">
-                    Регистрация открыта
-                  </div>
-
-                  <div className="z-0 relative w-50 h-50 flex-shrink-0 m-0 p-0 -ml-6 -mr-8">
-                    <img
-                      src="/assets/Qback.svg"
-                      alt="logo"
-                      className="absolute z-0 top-0 left-0 w-full h-full rounded-lg"
-                    />
-                    <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-6xl">
-                      {event.formattedDay}
-                    </div>
-                  </div>
-
-                  <div className="flex-1 space-y-2 text-white mt-4 z-10">
-                    <div className="flex items-center gap-4">
-                      <div className="text-2xl font-bold">{event.title}</div>
-                      <div className="text-xs text-gray-400">
-                        {format(parseISO(event.event_datetime.split('T')[0]), "d MMMM, EEEE", { locale: ru })}
+      {filteredEvents.map(
+        ({ month, events }) =>
+          events.length > 0 && (
+            <div key={month} className="mb-9">
+              <h2 className="text-2xl font-semibold mb-4 capitalize">
+                {month}
+              </h2>
+              <div className="space-y-4">
+                {events.map((event) => (
+                  <div
+                    key={event.id}
+                    className="relative bg-primary p-6 py-4 rounded-2xl shadow-lg flex gap-6"
+                  >
+                    {event.is_registration_open && (
+                      <div className="border-[0.5px] mx-4 px-4 py-2 rounded-sm absolute top-9 right-4 text-sm font-medium text-green-400 z-10">
+                        Регистрация открыта
+                      </div>
+                    )}
+                    {!event.is_registration_open && (
+                      <div className="border-[0.5px] mx-4 px-4 py-2 rounded-sm absolute top-9 right-4 text-sm font-medium text-red-400 z-10">
+                        Регистрация закрыта
+                      </div>
+                    )}
+                    {event.is_registration_open === null && (
+                      <div className="border-[0.5px] mx-4 px-4 py-2 rounded-sm absolute top-9 right-4 text-sm font-medium text-yellow-400 z-10">
+                        Неизвестно
+                      </div>
+                    )}
+                    <div className="z-0 relative w-50 h-50 flex-shrink-0 m-0 p-0 -ml-6 -mr-8">
+                      <img
+                        src="/assets/Qback.svg"
+                        alt="logo"
+                        className="absolute z-0 top-0 left-0 w-full h-full rounded-lg"
+                      />
+                      <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-6xl">
+                        {event.formattedDay}
                       </div>
                     </div>
+                    <div className="flex-1 space-y-2 text-white mt-4 z-10">
+                      <div className="flex items-center gap-4">
+                        <div className="text-2xl font-bold">{event.title}</div>
+                        <div className="text-xs text-gray-400">
+                          {format(
+                            parseISO(event.event_datetime.split("T")[0]),
+                            "d MMMM, EEEE",
+                            { locale: ru }
+                          )}
+                        </div>
+                      </div>
 
-                    <div className="flex flex-wrap gap-2 text-xs font-medium">
-                      <span className="bg-[#6F462A] px-4 py-1 rounded-sm text-sm font-m">Для студентов</span>
-                      <span className="bg-[#2A6F5E] px-4 py-1 rounded-sm text-sm font-m">Онлайн</span>
-                      <span className="bg-secondary px-4 py-1 rounded-sm text-sm font-m">БПФ</span>
+                      <div className="flex flex-wrap gap-2 text-xs font-medium">
+                        <span className="bg-[#6F462A] px-4 py-1 rounded-sm text-sm font-m">
+                          Для студентов
+                        </span>
+                        <span className="bg-[#2A6F5E] px-4 py-1 rounded-sm text-sm font-m">
+                          Онлайн
+                        </span>
+                        <span className="bg-secondary px-4 py-1 rounded-sm text-sm font-m">
+                          БПФ
+                        </span>
+                      </div>
+
+                      <div className="text-sm text-green-400 z-10">
+                        {event.cost === 0 ? "Бесплатно" : `${event.cost} KZT`}
+                      </div>
+                      <div className="text-sm text-gray-300 max-w-[450px]">
+                        {" "}
+                        г. {event.city}
+                      </div>
+                      <div className="text-sm text-gray-400">
+                        {event.description}
+                      </div>
                     </div>
-
-                    <div className="text-sm text-green-400 z-10">
-                      {event.cost === 0 ? "Бесплатно" : `${event.cost} KZT`}
+                    <div className="absolute right-4 bottom-6">
+                      <button className="bg-white text-black mx-4 px-6 py-2 rounded-md font-semibold">
+                        Подробнее
+                      </button>
                     </div>
-                    <div className="text-sm text-gray-300 max-w-[450px]"> г. {event.city}</div>
-                    <div className="text-sm text-gray-400">{event.description}</div>
                   </div>
-
-                  <div className="absolute right-4 bottom-6">
-                    <button className="bg-white text-black mx-4 px-6 py-2 rounded-md font-semibold">
-                      Подробнее
-                    </button>
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        )
-      ))}
+          )
+      )}
     </div>
   );
 };
