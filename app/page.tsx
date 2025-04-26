d5224c6d194daf3283ca8eca89d814ec24cfdb54
ondyy <ondyyvfx@gmail.com>
2025-04-26 23:42:52 +0500

    layout modification

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Hero from "@/components/shared/Hero";
 import Navbar from "../components/shared/Navbar";
 import UpcomingTournaments from "@/components/shared/UpcomingTournaments";
-import TopSpeakersSection from "@/components/shared/TopSpeakerSection";
 import DebateClubs from "@/components/shared/DebateClubs";
-
+import TopSpeakersSection from "@/components/shared/TopSpeakersSection";
 
 export default function Home() {
   return (
