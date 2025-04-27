47ef52908664e7e6086f1b4f95ca079f8bdc5e1a
ondyy <ondyyvfx@gmail.com>
2025-04-27 23:50:39 +0500

    cors config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
         protocol: "http",
         hostname: "localhost",
         port: "8000", // важно указать порт, если ты используешь его
-        pathname: "/avatars/**", // можешь указать '*' или конкретный путь
+        pathname: "/media/**", // можешь указать '*' или конкретный путь
       },
     ],
   },
