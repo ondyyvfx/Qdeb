c486a95dcd529fbf786944aa8520a88b7784816b
ondyy <ondyyvfx@gmail.com>
2025-05-01 23:02:15 +0500

    mobile v

--- app/calendar/ClientCalendar.tsx
+++ app/calendar/ClientCalendar.tsx
@@ -123,27 +123,21 @@ const ClientCalendar = ({ events, categories }: ClientCalendarProps) => {
   return (
     <div>
       {/* Фильтры */}
-      <div className="flex flex-col md:flex-row gap-4 justify-between items-start md:items-center mb-6">
+      <div className="flex flex-col sm:flex-row gap-4 justify-between items-stretch sm:items-center mb-6">
         <input
           type="text"
           placeholder="Поиск по названию"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="p-2 rounded-lg bg-dark-800 text-white w-1/3"
+          className="p-2 rounded-lg bg-dark-800 text-white sm:w-full w-1/3"
         />
         <select
           value={selectedCity}
           onChange={(e) => setSelectedCity(e.target.value)}
-          className="p-2 rounded-lg bg-primary text-white w-1/3 appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
-          style={{ color: "#fff" }}
+          className="p-2 rounded-lg bg-primary text-white sm:w-full w-1/3 appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
         >
           {uniqueCities.map((city) => (
-            <option
-              key={city}
-              value={city === "Все" ? "" : city}
-              style={{ color: "#fff" }}
-              className="sm:w-[100px]"
-            >
+            <option key={city} value={city === "Все" ? "" : city}>
               {city}
             </option>
           ))}
@@ -162,7 +156,7 @@ const ClientCalendar = ({ events, categories }: ClientCalendarProps) => {
                 {events.map((event) => (
                   <div
                     key={event.id}
-                    className="relative bg-primary p-6 py-4 rounded-2xl shadow-lg flex flex-col md:flex-row gap-6"
+                    className="relative bg-primary p-4 sm:p-6 py-4 rounded-2xl shadow-lg flex flex-col md:flex-row gap-4 sm:gap-6"
                   >
                     {event.is_registration_open && (
                       <div className="border-[0.5px] mx-4 sm:px-4 sm:py-2 rounded-sm absolute sm:top-9 sm:right-4 sm:text-sm font-medium text-green-400 z-10 px-2 py-1 top-4 right-2 text-xs">
@@ -179,13 +173,13 @@ const ClientCalendar = ({ events, categories }: ClientCalendarProps) => {
                         Неизвестно
                       </div>
                     )}
-                    <div className="relative w-28 h-28 md:w-50 md:h-50 flex-shrink-0 m-0 p-0 -ml-6 md:-mr-8">
+                    <div className="relative w-20 h-20 md:w-50 md:h-50 flex-shrink-0 m-0 p-0 -ml-4 md:-mr-8">
                       <img
                         src="/assets/Qback.svg"
                         alt="logo"
                         className="absolute z-0 top-0 left-0 w-full h-full rounded-lg"
                       />
-                      <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-6xl">
+                      <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-3xl sm:text-6xl">
                         <div className="flex flex-col items-center">
                           <span>
                             {format(parseISO(event.start_date), "dd")}
@@ -199,10 +193,28 @@ const ClientCalendar = ({ events, categories }: ClientCalendarProps) => {
                         </div>
                       </div>
                     </div>
-                    <div className="flex-1 space-y-2 text-white mt-4 z-10">
+                    <div className="sm:flex-1 sm:space-y-2 text-white flex flex-col sm:mt-4 z-10">
                       <div className="flex items-center gap-4">
-                        <div className="text-2xl font-bold">{event.title}</div>
-                        <div className="text-xs text-gray-400">
+                        <div className="text-lg sm:text-2xl font-bold">
+                          {event.title}
+                          <div className="sm:hidden text-xs text-gray-400 text-center mb-2">
+                            {format(
+                              parseISO(event.start_date.split("T")[0]),
+                              "d MMMM, EEEE",
+                              { locale: ru }
+                            )}
+                            {event.end_date &&
+                            event.end_date !== event.start_date
+                              ? ` - ${format(
+                                  parseISO(event.end_date),
+                                  "d MMMM, EEEE",
+                                  { locale: ru }
+                                )}`
+                              : null}
+                          </div>
+                        </div>
+                        {/* date with weekdays */}
+                        <div className="text-xs text-gray-400 hidden sm:flex">
                           {format(
                             parseISO(event.start_date.split("T")[0]),
                             "d MMMM, EEEE",
@@ -217,7 +229,7 @@ const ClientCalendar = ({ events, categories }: ClientCalendarProps) => {
                             : null}
                         </div>
                       </div>
-                      <div className="sm:flex flex-col gap-2 mt-4 hidden">
+                      <div className="flex flex-col gap-2 mt-4 hiddenn">
                         <div className="flex flex-wrap gap-2 text-xs font-medium">
                           {event.categories.map((category) => (
                             <span
@@ -243,20 +255,20 @@ const ClientCalendar = ({ events, categories }: ClientCalendarProps) => {
                         {activeEvent === event.id ? event.description : ""}
                       </div>
 
-                      <div className="absolute right-4 bottom-6 md:static flex flex-col justify-end md:flex-row gap-2 mt-4 md:mt-0">
+                      <div className="absolute right-4 bottom-6 md:static w-1/3 sm:w-full flex flex-row sm:flex-col justify-end md:flex-row gap-2 mt-4 md:mt-0">
                         {event.is_registration_open &&
                           event.registration_link && (
                             <a
                               href={event.registration_link}
                               target="_blank"
                               rel="noopener noreferrer"
-                              className="bg-green-400 text-white px-3 py-1 text-sm sm:text-[16px] sm:px-4 sm:py-2 rounded-md font-semibold hover:bg-green-500 transition"
+                              className="bg-green-400 text-white px-1 py-1 text-[11px] text-center sm:text-[16px] sm:px-4 sm:py-2 rounded-md font-semibold hover:bg-green-500 transition"
                             >
-                              Зарегистрироваться
+                              Регистрация
                             </a>
                           )}
                         <button
-                          className="bg-white text-black px-3 py-1 text-sm sm:text-[16px] sm:px-6 sm:py-2 rounded-md font-semibold"
+                          className="bg-white text-black px-3 py-1 text-[11px] sm:text-[16px] sm:px-6 sm:py-2 rounded-md font-semibold"
                           onClick={() =>
                             setActiveEvent(
                               activeEvent === event.id ? null : event.id
