b6906ad4304abe1cb8c468989c5096da0bf81c3c
ondyy <ondyyvfx@gmail.com>
2025-05-01 23:04:19 +0500

    mobile -v

--- components/shared/DebateClubs.tsx
+++ components/shared/DebateClubs.tsx
@@ -57,7 +57,7 @@ const debateClubs: DebateClub[] = [
 
 const DebateClubs = () => {
   return (
-    <section className="my-24 mx-19">
+    <section className="my-24 mx-5 sm:mx-19">
       <h2 className="text-3xl font-bold mb-6">Дебатные клубы</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {debateClubs.map((club, index) => (
