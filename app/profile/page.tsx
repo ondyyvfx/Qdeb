57aff18a21865343d6dd7b5bcbcde8c4a3c40cf6
ondyy <ondyyvfx@gmail.com>
2025-05-01 23:03:21 +0500

    mobile -v

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,11 @@
 import EditProfileForm from "./EditProfileForm";
+import Navbar from "@/components/shared/Navbar";
 
 export default function ProfilePage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start pt-12 px-4">
-      <h1 className="text-3xl font-bold  text-center">
+    <div className="min-h-screen flex flex-col items-center justify-start px-4">
+      <Navbar />
+      <h1 className="text-3xl font-bold pt-12 text-center">
         Редактирование профиля
       </h1>
       <EditProfileForm />
