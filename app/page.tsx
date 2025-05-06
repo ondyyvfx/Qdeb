6e8ae95488385ffcfbf16e3ee34a9c60f9d620b8
ondyy <ondyyvfx@gmail.com>
2025-05-06 17:57:53 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import Hero from "@/components/shared/Hero";
 import Navbar from "../components/shared/Navbar";
 import UpcomingTournaments from "@/components/shared/UpcomingTournaments";
 import DebateClubs from "@/components/shared/DebateClubs";
 import TopSpeakersSection from "@/components/shared/TopSpeakersSection";
 import Footer from "@/components/shared/Footer";
+import ResponsiveHero from "@/components/shared/ResponsiveHero";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
-      <Hero />
+      <ResponsiveHero />
       <UpcomingTournaments />
       <TopSpeakersSection />
       <DebateClubs />
