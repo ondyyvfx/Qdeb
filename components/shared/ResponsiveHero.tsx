13426c28a51f78abc066d5ce02b857315d179c15
ondyy <ondyyvfx@gmail.com>
2025-05-06 18:01:18 +0500

    update

---
+++ components/shared/ResponsiveHero.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Hero from "./Hero";
+import MobileHero from "./MobileHero";
+
+const ResponsiveHero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
+
+  return isMobile ? <MobileHero /> : <Hero />;
+};
+
+export default ResponsiveHero;
