0514de9245f7078521be0000e88ff9636d08cc73
ondyy <ondyyvfx@gmail.com>
2025-05-07 14:20:18 +0500

    responsive adjustments

--- components/shared/TopSpeakersSection.tsx
+++ components/shared/TopSpeakersSection.tsx
@@ -2,7 +2,7 @@ import SpeakerSlider from "./SpeakerSlider";
 
 const TopSpeakersSection = () => {
   return (
-    <section className="my-24 mx-4 sm:mx-19">
+    <section className="my-24 mx-4 md:mx-10 xl:mx-19">
       <h1 className="text-3xl font-bold mb-6">Лучшие спикеры</h1>
       <SpeakerSlider />
     </section>
