7cc65c6ab86376d2efe2c5e2d283b57d846b769a
ondyy <ondyyvfx@gmail.com>
2025-05-09 00:30:32 +0500

    avatar form update

--- components/shared/SpeakerCard.tsx
+++ components/shared/SpeakerCard.tsx
@@ -25,29 +25,35 @@ const SpeakerCard = ({
 }: SpeakerCardProps) => {
   console.log("Avatar src:", avatar);
   return (
-    <Card className="flex flex-col sm:flex-row items-start sm:items-center gap-10 p-10 bg-primary border-none">
-      <img
-        src={avatar}
-        alt={full_name}
-        className="w-24 h-24 sm:w-16 sm:h-16 rounded-full object-cover"
-      />
-      <CardContent className="flex flex-col p-0">
+    <Card className="relative flex flex-col w-[410px] sm:flex-row items-start sm:items-center gap-10 p-10 overflow-hidden bg-primary border-none ">
+      <div className="absolute -top-5 -left-5 w-40 h-40 rounded-full overflow-hidden z-10">
+        <img
+          src={avatar}
+          alt={full_name}
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <CardContent className="flex flex-col p-0 sm:ml-32">
         <h3 className="font-semibold text-lg">{full_name}</h3>
         <p className="text-sm mt-1">Рейтинг: {elo_rating}</p>
         <p className="text-sm mt-1">Средний балл: {avg_speech}</p>
         <p className="text-sm mt-1">
           Общее количество достижений: {total_achievements}
         </p>
-        {/* {achievements && achievements.length > 0 && (
-          <div className="text-sm text-muted-foreground mt-1">
-            {achievements.map((achievement) => (
-              <p key={achievement.tournament_id}>{achievement.title}</p>
-            ))}
-          </div>
-        )} */}
       </CardContent>
     </Card>
   );
 };
 
 export default SpeakerCard;
+
+{
+  /* {achievements && achievements.length > 0 && (
+          <div className="text-sm text-muted-foreground mt-1">
+            {achievements.map((achievement) => (
+              <p key={achievement.tournament_id}>{achievement.title}</p>
+            ))}
+          </div>
+        )} */
+}
