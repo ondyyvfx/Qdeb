2db56c8790afde5773472fbd3819f1c5d664231c
ondyyvfx <ondyyvfx@gmail.com>
2025-11-30 00:23:01 +0500

    typo topspeakers section

--- components/shared/TopSpeakersSection.tsx
+++ components/shared/TopSpeakersSection.tsx
@@ -5,9 +5,8 @@ const TopSpeakersSection = () => {
         <section className="my-24 mx-4 md:mx-10 xl:mx-19">
             <h1 className="text-3xl font-bold mb-6">Лучшие спикеры</h1>
             <p className="text-center text-red-400">
-                В данный момент эта функция находиться на стадии разработки
+                В данный момент эта функция находится на стадии разработки
             </p>
-            {/* <SpeakerSlider /> */}
         </section>
     );
 };
